376085b4414667b590acaacbd2929b0852853c72
Dinisimys <dinisimys2018@gmail.com>
2021-09-12 22:05:18 +0300

    init 2

--- public/index.php
+++ public/index.php
@@ -8,8 +8,7 @@ define('GF_START_TIME',microtime(1));
 
 app()->init();
 
-dd(QueryBuilder::setDTO(Contact::class))->w
-dd(\App\Database\DTO\Contact::whereNotNull('email')->get());
+
 
 //app(\GF\HTTP\Handler::class)->run();
 
